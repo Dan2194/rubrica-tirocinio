1b7bc520819a1658b27d89c4b1a0bc3b45556ce0
Dan2194 <85521396+Dan2194@users.noreply.github.com>
2026-02-27 01:53:46 +0100

    Add files via upload

--- schema_database.sql
+++ schema_database.sql
@@ -1,17 +1,16 @@
 -- Creazione del database
 CREATE DATABASE IF NOT EXISTS rubrica_db;
 
--- Selezione del database da utilizzare
 USE rubrica_db;
 
--- Creazione della tabella per gli utenti
+-- tabella per gli utenti
 CREATE TABLE IF NOT EXISTS utenti (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
     );
 
--- Creazione della tabella per le persone
+-- tabella per le persone
 CREATE TABLE IF NOT EXISTS persone (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(50) NOT NULL,
@@ -21,5 +20,5 @@ CREATE TABLE IF NOT EXISTS persone (
     eta INT
     );
 
--- Inserimento di un utente di test per permettere il primo accesso al software
+-- utente di test
 INSERT INTO utenti (username, password) VALUES ('admin', 'admin');
